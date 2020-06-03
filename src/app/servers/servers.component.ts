28d0b7479c5633f8efe57a392552f2fdba1d44dc
victorval <vector386@gmail.com>
2020-06-03 12:10:01 -0500

    styling elements dinamically with ngStyle, comments removed

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -25,7 +25,6 @@ export class ServersComponent implements OnInit {
   }
 
   onUpdateServerName(event: Event) {
-    // console.log(event);
     this.serverName = (<HTMLInputElement>event.target).value;
   }
 
