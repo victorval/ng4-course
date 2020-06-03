78ef9100062b8aa87f52e5586a694dc3b23b98ec
victorval <vector386@gmail.com>
2020-06-03 12:15:01 -0500

    ngIf like conditional implemented in ngClass for styling

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,21 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+  .online {
+    color: white;
+  }`]
 })
-export class ServerComponent  {
+export class ServerComponent {
   serverId = 10;
   serverStatus = 'offline';
 
-  constructor(){
+  constructor() {
     this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
   }
 
-  getServerStatus(){
+  getServerStatus() {
     return this.serverStatus;
   }
 
-  getColor(){
+  getColor() {
     return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
