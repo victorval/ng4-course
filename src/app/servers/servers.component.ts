09081913b5c55d046e5a13df9145dbfb70926b7d
victorval <vector386@gmail.com>
2020-06-02 21:31:35 -0500

    event binding for button in order to change status of the server

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'no server was created!';
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -13,6 +15,10 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'server was created!';
   }
 
 }
