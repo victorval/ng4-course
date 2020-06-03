a9784ec6802275e28d8cceac32854a48fb72f4bb
victorval <vector386@gmail.com>
2020-06-02 20:49:56 -0500

    played with components and how to bind the selector, styles and templates

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-server',
+  //selector: '[app-server]',
+  selector: '.app-server',
   template: '<p>The server component</p>',
   styleUrls: ['./server.component.css']
 })
