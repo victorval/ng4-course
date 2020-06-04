c0cf94476cd721e623cca558e6547e29ef75f626
victorval <vector386@gmail.com>
2020-06-04 15:33:58 -0500

    shopping edit  added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,6 +10,9 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A test Recipe',
     'this is simple test recipe',
+    'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg'),
+    new Recipe('A test Recipe',
+    'this is simple test recipe',
     'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg')
   ];
 
