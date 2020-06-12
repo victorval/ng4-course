2a7978774f4cc93e33083a2d702eb6af3e6fec84
victorval <vector386@gmail.com>
2020-06-12 14:28:01 -0500

    fix directive for the closure from anywhere

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,11 +1,12 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import { Directive, HostListener, HostBinding, ElementRef } from '@angular/core';
 
 @Directive({
     selector: '[appDropDown]'
 })
 export class DropDownDirective {
     @HostBinding('class.open') isOpen = false;
-    @HostListener('click') toggleOpen() {
-        this.isOpen = !this.isOpen;
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
     }
+    constructor(private elRef: ElementRef) { }
 }
