d6ba205ddf29bcf3748e840222035569630c14a1
victorval <vector386@gmail.com>
2020-06-24 18:23:52 -0500

    retrieving of the ingredients of recipe and set the data into the placeholder

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,16 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
         new Recipe('A test Recipe',
             'this is simple test recipe',
-            'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg'),
+            'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg', [new Ingredient('meat', 1)]),
         new Recipe('A test Recipe',
             'this is simple test recipe',
-            'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg')
+            'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/spaghetti-puttanesca_1.jpg', [new Ingredient('meat', 1)])
     ];
     getRecipes() {
         return this.recipes.slice();
